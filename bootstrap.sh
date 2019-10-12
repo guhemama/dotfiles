74950098b2fc6a7d7730f54d18d5be460ddd10b6
Gustavo Henrique Mascarenhas Machado <guhemama@gmail.com>
2019-10-12 17:52:40 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,12 @@ mkdir ~/.local/bin/
 
 # Essentials & dependencies
 sudo apt-get install build-essential
-sudo apt-get install gdebi htop tmux unrar unzip gimp transmission vim curl filezilla git vlc tmux httpie
-sudo apt-get install numix-icon-theme numix-icon-theme-circle
+sudo apt-get install gdebi htop tmux unrar unzip gimp vim curl filezilla git vlc tmux httpie
 
 # PHP
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get install php7.2-cli php7.2-bcmath php7.2-curl php7.2-gd php7.2-intl php7.2-json php7.2-mbstring php7.2-pdo php7.2-mysql php7.2-pgsql php7.2-readline php7.2-sqlite3 php7.2-tidy php7.2-zip php7.2-xml
+sudo apt-get install php7.3-cli php7.3-bcmath php7.3-curl php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-pdo php7.3-mysql php7.3-pgsql php7.3-readline php7.3-sqlite3 php7.3-tidy php7.3-zip php7.3-xml
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mv composer.phar .local/bin/composer
@@ -26,7 +24,7 @@ sudo apt-get install sublime-text
 # Java
 sudo add-apt-repository ppa:linuxuprising/java
 sudo apt update
-sudo apt install oracle-java10-installer
+sudo apt install oracle-java13-installer
 
 # Scala
 ## SBT
@@ -38,38 +36,15 @@ sudo apt-get install sbt
 ## Ammonite
 sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm
 
-# Redis
-sudo add-apt-repository ppa:chris-lea/redis-server
-sudo apt-get update
-sudo apt-get install redis
-
-# Node & Yarn
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install -y nodejs yarn
-
 # Ruby & RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-# MySQL
-sudo apt-get install mysql-client-5.7 mysql-server-5.7 mysql-workbench
-
-# Postgres
-echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee -a /etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install postgresql-10 postgresql-client-10 pgadmin4
-sudo apt-get -y install python3-pip
-pip3 install pgcli
 
 # Ansible
 pip3 install ansible
 
 # SQLite
-sudo apt-get install sqlite3 sqliteman
+sudo apt-get install sqlite3
 
 # Clean everything
 sudo apt-get autoremove
